ae82477779326b45a0b7e932934f433fc0222cd0
Connor Roizman <connorjayr@gmail.com>
2016-07-16 01:55:24 -0400

    Add offline player providers and fix wildcard matches

--- src/main/java/ee/ellytr/command/argument/provider/minecraft/OfflinePlayersProvider.java
+++ src/main/java/ee/ellytr/command/argument/provider/minecraft/OfflinePlayersProvider.java
@@ -1,24 +1,9 @@
-/*
- * This file is part of EllyCommand.
- *
- * EllyCommand is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * EllyCommand is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with EllyCommand.  If not, see <http://www.gnu.org/licenses/>.
- */
 package ee.ellytr.command.argument.provider.minecraft;
 
 import ee.ellytr.command.argument.ArgumentProvider;
-import ee.ellytr.command.util.Players;
+import ee.ellytr.command.util.OfflinePlayers;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -27,23 +12,23 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-public class PlayersProvider implements ArgumentProvider<Players> {
+public class OfflinePlayersProvider implements ArgumentProvider<OfflinePlayers> {
 
   @Override
-  public Players getMatch(String in, CommandSender sender) {
-    Players players = new Players();
+  public OfflinePlayers getMatch(String in, CommandSender sender) {
+    OfflinePlayers offlinePlayers = new OfflinePlayers();
     if (in.contains(" ")) {
       for (String player : in.split(" ")) {
-        players.addAll(getPlayers(player));
+        offlinePlayers.addAll(getOfflinePlayers(player));
       }
     } else {
-      players.addAll(getPlayers(in));
+      offlinePlayers.addAll(getOfflinePlayers(in));
     }
-    return players.isEmpty() ? null : players;
+    return offlinePlayers.isEmpty() ? null : offlinePlayers;
   }
 
-  private List<Player> getPlayers(String in) {
-    List<Player> players = new ArrayList<>();
+  private List<OfflinePlayer> getOfflinePlayers(String in) {
+    List<OfflinePlayer> players = new ArrayList<>();
 
     boolean exact = in.startsWith("@");
     boolean uuid = in.startsWith("#");
@@ -65,10 +50,10 @@ public class PlayersProvider implements ArgumentProvider<Players> {
           }
         }
       } else {
-        players.add(Bukkit.getPlayer(UUID.fromString(in)));
+        players.add(Bukkit.getOfflinePlayer(UUID.fromString(in)));
       }
     } else if (exact) {
-      players.add(Bukkit.getPlayerExact(in));
+      players.add(Bukkit.getOfflinePlayer(in));
     } else {
       if (containsWildcard) {
         for (Player player : Bukkit.getOnlinePlayers()) {
